5b41704e1728b7ce5d142fe0656671adf2c9586f
Kevin Nguy <kevnguy@gmail.com>
2014-03-15 12:17:03 -0700

    Empty project

---
+++ Sail/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  Sail
+//
+//  Created by Kevin Nguy on 3/15/14.
+//  Copyright (c) 2014 Kevin Nguy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
